e6ba849038f8e070982918b128747b4995aee91c
skimran2708 <imranuddin.shaik@zemosolabs.com>
2021-12-01 04:01:21 +0530

    Spring Boot Thymeleaf Topic

---
+++ 09-Spring-Boot-Thymeleaf/thymeleaf-demo-employees-list-db/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.springboot.thymeleafdemo.dao;
+
+import com.example.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+}
